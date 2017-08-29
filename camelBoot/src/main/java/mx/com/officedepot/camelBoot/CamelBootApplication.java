759246cd62f58f45b95533d7b95bf55b68b37062
Héctor Castillo <hgcr89@gmail.com>
2017-08-29 12:26:43 -0500

    Create camelBoot

---
+++ camelBoot/src/main/java/mx/com/officedepot/camelBoot/CamelBootApplication.java
@@ -0,0 +1,12 @@
+package mx.com.officedepot.camelBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelBootApplication.class, args);
+	}
+}
